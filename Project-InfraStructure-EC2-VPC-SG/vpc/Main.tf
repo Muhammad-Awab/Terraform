6304cc5eb93e87f57de66a114a3670214cadaa6a
Muhammad-Awab <muhammadawab82002@gmail.com>
2023-07-25 07:01:55 +0000

    Make complete project Infrastructure

---
+++ Project-InfraStructure-EC2-VPC-SG/vpc/Main.tf
@@ -0,0 +1,50 @@
+resource "aws_vpc" "project-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+    tags = {
+        Name = "project-vpc"
+    }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id= aws_vpc.project-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
+resource "aws_internet_gateway" "project-igw" {
+  vpc_id = aws_vpc.project-vpc.id
+
+  tags = {
+    Name = "project-igw"
+  }
+}
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.project-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.project-igw.id
+  }
+  tags = {
+    Name = "public-route-table"
+  }
+}
+resource "aws_route_table_association" "public-route-table-association" {
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_subnet" "private_subnet" {
+    vpc_id = aws_vpc.project-vpc.id  
+    cidr_block = "10.0.2.0/24"
+    availability_zone = var.availability_zone
+    map_public_ip_on_launch = false
+    tags = {
+        Name = "private_subnet"
+    }
+}
